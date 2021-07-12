e9e16ea4827b60aeb8bada86e458d2ba6940b032
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-07-12 17:08:57 -0400

    recursive ranger problem

---
+++ Recursion-challenges/recursiveRange.js
@@ -0,0 +1,12 @@
+// write a function called recursiveRange which accepts a number and adds up all the numbers
+// from 0 to the number passed to the function 
+
+function recursiveRange(){
+
+    if (num === 0) return 0;
+
+    num + recursiveRange(num-1)
+
+}
+
+recursiveRange(6);
