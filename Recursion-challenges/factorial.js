6151060e0e35b84e898b215441923459f307b672
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-07-12 17:01:25 -0400

    factorial problem set

---
+++ Recursion-challenges/factorial.js
@@ -0,0 +1,13 @@
+// write a function factorial which accepts  a number and returns the factorial of the number
+// a facotiral is the product of an integer and all the integers below it
+
+
+function factorial() {
+
+    if (num === 1) return 1;
+
+    return num * factorial(num-1)
+
+}
+
+factorial(4)
