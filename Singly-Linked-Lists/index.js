5f562cae611998e1efe90f3e4fd0d36e62894588
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-09-02 10:22:37 -0400

    Singly Link List pop

---
+++ Singly-Linked-Lists/index.js
@@ -0,0 +1,43 @@
+// node stores a piece of data - val
+//reference to next node - next
+
+// resources : https://www.freecodecamp.org/news/implementing-a-linked-list-in-javascript/
+// https://medium.com/swlh/singly-linked-list-in-javascript-a0e58d045561
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next - null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+// let first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you?");
+
+const list = new SinglyLinkedList();
+list.push("HELLO");
+list.push("Goodbye");
